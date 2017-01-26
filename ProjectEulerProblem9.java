de3a5a79632449a3660f111d16f14e53fd79403b
adprocas <adam@leathorn.com>
2017-01-26 18:32:09 -0500

    Format

--- ProjectEulerProblem9.java
+++ ProjectEulerProblem9.java
@@ -8,19 +8,23 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
  */
 package projecteulerproblem9;
+
 public class ProjectEulerProblem9 {
+
     public static void main(String[] args) {
-        for(int c = 997; c > 0; c--) {
-            for(int b = 2; b < c; b++) {
-                for(int a = 1; a+b+c <= 1000; a++) {
-                    if((a + b + c == 1000) && ((a*a) + (b*b) == (c*c))) {
-                        long result = a*b*c;
+        for (int c = 997; c > 0; c--) {
+            for (int b = 2; b < c; b++) {
+                for (int a = 1; a + b + c <= 1000; a++) {
+                    if ((a + b + c == 1000) && ((a * a) + (b * b) == (c * c))) {
+                        long result = a * b * c;
+
                         System.out.println(result);
+
                         return;
                     }
                 }
             }
         }
     }
-    
+
 }
